d8aa5fa329877bc783b04b51448517203d729b75
Batukhan0 <batuhankaragol0@gmail.com>
2024-12-09 19:57:25 +0100

    Get Started

---
+++ Get Started/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Get_Started.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
